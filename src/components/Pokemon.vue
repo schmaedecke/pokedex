99a2ff674a5a8f2b630daf10df23242b4b2313c8
Gustavo Luis Schmaedecke <97gustavo.luis@gmail.com>
2023-07-12 18:16:19 -0300

    Trocando sprite

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -4,7 +4,7 @@
     <div class="card">
       <div class="card-image">
         <figure>
-          <img :src="pokemon.front" alt="Placeholder image" />
+          <img :src="currentImg" alt="Placeholder image" />
         </figure>
       </div>
       <div class="card-content">
@@ -15,7 +15,11 @@
           </div>
         </div>
 
-        <div class="content"></div>
+        <div class="content">
+          <button class="button is-medium" @click="mudarSprite">
+            Mudar sprite
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -29,11 +33,14 @@ export default {
       this.pokemon.type = res.data.types[0].type.name;
       this.pokemon.front = res.data.sprites.front_default;
       this.pokemon.back = res.data.sprites.back_default;
+      this.currentImg = this.pokemon.front;
       console.log(this.pokemon);
     });
   },
   data() {
     return {
+      isFront: true,
+      currentImg: "",
       pokemon: {
         type: "",
         front: "",
@@ -50,6 +57,15 @@ export default {
     upper: function (value) {
       const newName = value[0].toUpperCase() + value.slice(1);
       return newName;
+    },
+    mudarSprite: function () {
+      if (this.isFront) {
+        this.isFront = false;
+        this.currentImg = this.pokemon.back;
+      } else {
+        this.isFront = true;
+        this.currentImg = this.pokemon.front;
+      }
     },
   },
 };
